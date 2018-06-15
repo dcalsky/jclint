381d8c18fbebc17582c7611d940530da71e3412d
wzes <793890838@qq.com>
2018-06-15 19:25:34 +0800

    remove the useless file

--- test/case.js
+++ test/case.js
@@ -8,14 +8,15 @@ const caseFolder = path.join(__dirname, "cases");
 const filenames = fs.readdirSync(caseFolder);
 
 function exec(input) {
-  parser.parse(input);
+  return parser.parse(input);
 }
 
 describe("Cases", function() {
   filenames.forEach(name => {
     it(`should pass the \x1b[32m${name.toUpperCase()}\x1b[0m case without error`, function(done) {
       fs.readFile(path.resolve(caseFolder, name), (err, data) => {
-        exec(data.toString());
+        let output = exec(data.toString());
+        console.log('exec result: ' + output);
         done();
       });
     });
