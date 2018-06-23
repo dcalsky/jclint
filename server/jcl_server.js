01074d7d3e49038abec2d8f4ded56e37313888c1
wzes <793890838@qq.com>
2018-06-23 13:52:53 +0800

    fixed the server ip bugs

--- server/jcl_server.js
+++ server/jcl_server.js
@@ -7,28 +7,36 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var hostname = '10.60.45.8'
+var port = 8800
+var cookie = 'ltpaToken2=/7SwzNONpkfjvgOoo+qGiL1KPEjgJTmJuRipSA8JVgXTUqn6W1cqgezIe7bpmgmlto9KddXbQW8qVnpylwVCt9vBCCkE+vJDH2OGs+2NCsOTjKnCcShiqWQY5pjwELSVQeVuID+xJBeGK2bbOt3P2wzPo6kBh7cJka0xt1b8M4F7oAD8ielih5QIU1Cc8nC2THxFAJlUbrOjTK5+kr/GEbtp+S7yJTzy6I3zH2f/K+1E/f/V5s7wbM4dDV/QDLzmk2k6Xx/6siSJixnaZjM9WotAkRW8xvjxGJiZHWdyhZs92oMg1a8CErBZ1Oo3WmFb'
+
 // auth
 app.post('/auth', function (req, res) {
-    console.log(req.body.content)
     var userid = req.body.userid;
     var password = req.body.password;
-    var b = new Buffer('Basic ' + userid + ':' + password);
+    var b = new Buffer(userid + ':' + password);
     var base64string = b.toString('base64');
+    console.log(base64string)
     var options = {  
-        hostname: '10.60.48.5',  
-        port: 8800,  
+        hostname: hostname,  
+        port: port,  
         path: '/zosmf',  
-        method: 'GET',  
+        rejectUnauthorized: false,
+        method: 'POST',  
         headers:{
-            'Authorization': base64string
+            'Authorization': 'Basic ' + base64string
         }  
     }
     var req = https.request(options, function(res) {  
         console.log('Status:', res.statusCode);  
         console.log('headers:', JSON.stringify(res.headers));  
+        console.log('Cookie:', res.headers['set-cookie'][0].split(';')[0])
+        cookie = res.headers['set-cookie'][0].split(';')[0]
         res.setEncoding('utf-8');  
         res.on('data', function(chun) {  
             // get cookies
+            console.log(chun)
         });  
         res.on('end', function() {  
 
@@ -37,8 +45,8 @@ app.post('/auth', function (req, res) {
     req.on('error',function(err) {  
         console.error(err);  
     });
-    req.end(); 
-    res.send('Hello World');
+    req.end();
+    res.send('HELLO');
 });
 
 
@@ -47,13 +55,14 @@ app.post('/zos/job', function (req, res) {
     console.log(req.body.content)
     var postData = req.body.content;
     var options = {  
-        hostname: '10.60.48.5',  
+        hostname: hostname,  
         port: 8800,  
         path: '/zosmf/restjobs/jobs',  
+        rejectUnauthorized: false,
         method: 'PUT',  
         headers:{
         'Content-Type':'text/plain',
-        'Cookies': ''
+        'Cookie': cookie
         }  
     }
     var req = https.request(options, function(res) {  
@@ -62,8 +71,9 @@ app.post('/zos/job', function (req, res) {
         res.setEncoding('utf-8');  
         res.on('data', function(chun) {  
             // get data
+            console.log(chun);  
         });  
-        res.on('end', function() {  
+        res.on('end', function(err) { 
             
         });  
     });    
@@ -72,17 +82,19 @@ app.post('/zos/job', function (req, res) {
     });  
     req.write(postData);  
     req.end();
+    res.send('HELLO');
 });
 
 // get the job status
 app.get('/zos/job/:job_name/:job_id', function (req, res) {
     var options = {  
-        hostname: '10.60.48.5',  
+        hostname: hostname,  
         port: 8800,  
+        rejectUnauthorized: false,
         path: '/zosmf/restjobs/jobs/' + req.params.job_name + '/' + req.params.job_id,  
         method: 'get',  
         headers: {
-            'Cookies': ''
+            'Cookies': cookie
         }  
     }
     var req = https.request(options, function(res) {  
@@ -102,18 +114,20 @@ app.get('/zos/job/:job_name/:job_id', function (req, res) {
     });  
     req.write(postData);  
     req.end();
+    res.send('HELLO');
 });
 
 // get the detail output
 app.get('/zos/job/:job_name/:job_id/files/:id/records', function (req, res) {
     var options = {  
-        hostname: '10.60.48.5',  
+        hostname: hostname,  
         port: 8800,  
+        rejectUnauthorized: false,
         path: '/zosmf/restjobs/jobs/' + req.params.job_name + '/' + req.params.job_id + 
             + '/files/' + req.params.id  + '/records',  
         method: 'get',  
         headers: {
-            'Cookies': ''
+            'Cookies': cookie
         }  
     }
     var req = https.request(options, function(res) {  
@@ -132,18 +146,19 @@ app.get('/zos/job/:job_name/:job_id/files/:id/records', function (req, res) {
     });  
     req.write(postData);  
     req.end(); 
-    res.send('Hello World');
+    res.send('HELLO');
 });
 // get the list of output
 app.get('/zos/job/:job_name/:job_id/files', function (req, res) {
     var options = {  
-        hostname: '10.60.48.5',  
+        hostname: hostname,  
         port: 8800,  
+        rejectUnauthorized: false,
         path: '/zosmf/restjobs/jobs/' + req.params.job_name + '/' + req.params.job_id + 
             + '/files',  
         method: 'get',  
         headers: {
-            'Cookies': ''
+            'Cookies': cookie
         }  
     }
     var req = https.request(options, function(res) {  
@@ -162,11 +177,11 @@ app.get('/zos/job/:job_name/:job_id/files', function (req, res) {
     });  
     req.write(postData);  
     req.end(); 
-    res.send('Hello World');
+    res.send('HELLO');
 });
  
-var server = app.listen(8081, function () {
- 
+var server = app.listen(8084, function () {
+
     var host = server.address().address
     var port = server.address().port
     
