765fddeef3304a2b9bc8f014270bb94497728316
dcalsky <dcalsky@163.com>
2018-06-26 17:51:06 +0800

    feat: adjust playground and integrate error message

--- playground/index.js
+++ playground/index.js
@@ -2,47 +2,66 @@ import debounce from "lodash/debounce";
 import CodeMirror from "codemirror";
 import Parser from "../src/jcl.parser";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/3024-day.css";
+import "./styles/3024-day.css";
+import "codemirror/addon/lint/lint.css";
 import "./styles/main.less";
 
 class Editor {
-  constructor(editor_id, output_id, options) {
-    const $container = document.getElementById(editor_id);
-    this.parser = new Parser();
-    this.code_editor = CodeMirror.fromTextArea($container, {
+  constructor(editorId, outputId, options) {
+    this.lineHandles = [];
+    const $container = document.getElementById(editorId);
+    const codeEditorOptions = {
       lineNumbers: true,
-      mode: "jcl",
-      matchBrackets: true,
-      theme: "3024-day"
-    });
+      gutters: ["CodeMirror-linenumbers", "CodeMirror-lint-markers"],
+      theme: "3024-day",
+      lint: true
+    };
+    this.parser = new Parser();
+    this.codeEditor = CodeMirror.fromTextArea($container, codeEditorOptions);
     this.$gutter = document.querySelector(".CodeMirror-gutters");
-
-    this.$output = document.getElementById(output_id);
+    this.$output = document.getElementById(outputId);
     this.initialize();
   }
   validate() {
-    this.parser.parse(this.code_editor.getValue());
-    if (this.parser.error) {
-      const body = this.parser.error;
-      this.$gutter.classList.toggle("error", true);
-      this.code_editor.addLineClass(
-        body.location.first_line - 1,
-        "gutter",
-        "error-line"
-      );
+    this.clear_marks();
+    this.parser.parse(this.codeEditor.getValue());
+    const errors = this.parser.errors;
+    if (errors.length !== 0) {
+      this.toggle_gutter_error(true);
+      this.handleErrors(errors);
     } else {
-      this.$output.textContent = "";
-      this.clear_gutter_mark();
+      this.toggle_gutter_error(false);
     }
   }
-  clear_gutter_mark() {
-    this.$gutter.classList.toggle("error", false);
-    this.code_editor.eachLine(line => {
-      this.code_editor.removeLineClass(line, "gutter", "error-line");
+  handleErrors(errors) {
+    errors.forEach(err => {
+      const { first_line, last_line, first_column, last_column } = err.location;
+      const lineHandle = this.codeEditor.getLineHandle(first_line - 1);
+      const marker = this.codeEditor.markText(
+        CodeMirror.Pos(first_line - 1, first_column),
+        CodeMirror.Pos(last_line - 1, last_column),
+        {
+          className: "CodeMirror-lint-mark-error",
+          title: "error"
+        }
+      );
+      this.lineHandles.push(lineHandle);
+      this.codeEditor.addLineClass(lineHandle, "gutter", "error-line");
     });
   }
+  clear_marks() {
+    this.lineHandles.forEach(line => {
+      this.codeEditor.removeLineClass(line, "gutter", "error-line");
+    });
+    this.codeEditor.getAllMarks().forEach(marker => {
+      marker.clear();
+    });
+  }
+  toggle_gutter_error(status = true) {
+    this.$gutter.classList.toggle("error", status);
+  }
   initialize() {
-    this.code_editor.on("changes", debounce(this.validate.bind(this), 500));
+    this.codeEditor.on("changes", debounce(this.validate.bind(this), 500));
   }
 }
 
